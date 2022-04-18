a0dbb9538737e3f689a43327fb12bdc3de572bd0
brytebee <brytebee@gmail.com>
2022-04-18 14:19:26 +0100

    :white_check_mark: Add changes requested by reviewer
    Added changes requested by reviewer

--- src/classes/genre.rb
+++ src/classes/genre.rb
@@ -2,14 +2,14 @@ class Genre
   attr_accessor :name
   attr_reader :id, :items
 
-  def initialize(name, items, id = rand(0..500))
+  def initialize(name)
     @name = name
-    @items = items
-    @id = id
+    @items = []
+    @id = rand(0..500)
   end
 
   def add_item(item)
-    @item << item
+    @item << item unless @item.include?(item)
     items.genre = self
   end
 end
