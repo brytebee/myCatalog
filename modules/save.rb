419bb3eed143213bf4e0aa8eb8b42fe73faa3c82
brytebee <brytebee@gmail.com>
2022-04-19 22:08:43 +0100

    :white_check_mark: Add save music album

--- modules/save.rb
+++ modules/save.rb
@@ -1,2 +1,10 @@
 module PreserveMusicAlbumGenre
+  def save_music_album
+    data = []
+    @music_albums.each do |music_album|
+      data << { pulish_date: music_album.publish_date, on_spotify: music_album.on_spotify }
+    end
+
+    File.write('./store/music_album.json', JSON.generate(data))
+  end
 end
