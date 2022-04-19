ad95688df3cfbb264e6cd747053710790f1f62f9
VuDej <drvu47@gmail.com>
2022-04-19 18:35:12 +0200

    fix errors linter

--- classes/label.rb
+++ classes/label.rb
@@ -1,13 +1,15 @@
 require_relative 'item'
 
-class Label
+class Label < Book
   attr_accessor :title, :color, :items
   attr_writer :id
 
   def initialize(title, color)
+    super(publish_date, publisher, cover_state)
     @id = Random.rand(1..1000)
     @title = title
     @color = color
+    @publish_date = publish_date
     @items = []
   end
 
--- modules/list_labels.rb
+++ modules/list_labels.rb
@@ -1,11 +1,12 @@
 module LabelList
+  include BookList
   def list_all_labels
     if @labels.length.zero?
       puts 'Database is empty'
     else
       puts 'List of Labels: '
       @labels.each_with_index do |label, index|
-        puts "#{index}: Label: Title: #{label.title} Color: #{label.color}"
+        puts "#{index}: Label: Title: #{label.title}, Color: #{label.color}"
       end
 
     end
