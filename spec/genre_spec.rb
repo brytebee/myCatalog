3feeb7033d36bad41ab2f5bf21dad69895d3748d
brytebee <brytebee@gmail.com>
2022-04-19 11:56:39 +0100

    :twisted_rightwards_arrows: Merge conflicts
    Merge conflicts

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,54 +1,52 @@
-require './src/classes/genre'
-require './src/classes/item'
+require './classes/genre'
+require './classes/item'
 require 'date'
 
 describe Genre do
-  context 'create an instance of Genre class' do
-    gen = Genre.new('Fiction')
-    today = Date.today
-    twelve_years = Date.new(today.year - 12, today.month, today.day)
-    item = Item.new(twelve_years, true)
-    item2 = Item.new(today, true)
-    p gen
+  context 'tests the Genre class' do
+    before(:all) do
+      @gen = Genre.new('Fiction')
+      today = Date.today
+      twelve_years = Date.new(today.year - 12, today.month, today.day)
+      @item = Item.new(twelve_years)
+      @item2 = Item.new(today)
+    end
 
     it 'should return an instance of Genre class' do
-      expect(gen.instance_of?(Genre)).to be(true)
+      expect(@gen.instance_of?(Genre)).to be(true)
     end
 
     it 'return the length of items' do
-      expect(gen.items.length).to be(0)
+      expect(@gen.items.length).to be(0)
     end
-    
-    # it 'return the length of items' do
-    #   gen.add_item=(item)
-    #   expect(gen.items.length).to eq(1)
-    # end
+
+    it 'return the length of items' do
+      @gen.add_item(@item)
+      expect(@gen.items.length).to eq(1)
+    end
+
+    it 'object is not an instance of class Item' do
+      expect(@gen).not_to be_kind_of Item
+    end
 
     it 'instance of name' do
-      instance_property = gen.name
+      instance_property = @gen.name
       expect(instance_property).to eq('Fiction')
     end
     
     it 'instance of name' do
-      instance_property = gen.id
+      instance_property = @gen.id
       expect(instance_property).not_to eq(-100)
     end
+
+    it 'Reject duplicate and still return length == 1' do
+      f = @gen.add_item(@item)
+      expect(@gen.items.length).to eq(1)
+    end
     
-    it 'instance of name' do
-      instance_property = gen.items
-      expect(instance_property).to eq([])
-    end
-
-    it 'Add item' do
-      f = gen.add_item=(item)
-      p f
-      expect(gen.items.length).to eq(1)
-    end
-
-    it 'return the length of items' do
-       f = gen.add_item=(item2)
-      p f
-      expect(gen.items.length).to be(2)
+    it 'Accept another item' do
+      f = @gen.add_item(@item2)
+      expect(@gen.items.length).to eq(2)
     end
   end
 end
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,25 +1,35 @@
-# require './src/classes/music_album'
-# require 'date'
+require './classes/music_album'
+require 'date'
 
-# describe MusicAlbum do
-#   context 'testing the MusicAlbum class' do
-#     # before(:each) do
-#     #   @today = Date.today
-#     #   @twelve_years = Date.new(@today.year - 12, @today.month, @today.day)
-#     #   @album1 = MusicAlbum.new(@today, false)
-#     #   @album2 = MusicAlbum.new('@twelve_years', 'true')
-#     # end
+describe MusicAlbum do
+  context 'testing the MusicAlbum class' do
+    before(:each) do
+      @today = Date.today
+      @twelve_years = Date.new(@today.year - 12, @today.month, @today.day)
+      @album1 = MusicAlbum.new(@today, false)
+      @album2 = MusicAlbum.new(@twelve_years, true)
+    end
 
-#     today = Date.today
-#     album1 = MusicAlbum.new(today, true)
+    it 'should return the MusicAlbum object' do
+      expect(@album1.instance_of?(MusicAlbum)).to be(true)
+    end
 
-#     it 'should take 3 parameters and return the MusicAlbum object' do
-#       expect(album1.instance_of?(MusicAlbum)).to be(false)
-#       # expect(@album2.instance_of?(MusicAlbum)).to be(true)
-#     end
+    it 'should use service return true' do
+      expect(@album2.instance_of?(MusicAlbum)).to be(true)
+    end
 
-#     # it 'should use service return true' do
-#     #   expect(@teacher1.can_use_services?).to be(true)
-#     # end
-#   end
-# end
+    it 'object is a type of class Item' do
+      expect(@album2).to be_kind_of Item
+    end
+
+    it 'instance of publish date property' do
+      instance_property = @album2.publish_date
+      expect(instance_property).to be(@twelve_years)
+    end
+    
+    it 'instance of on spotify property' do
+      instance_property = @album1.on_spotify
+      expect(instance_property).to eq(false)
+    end
+  end
+end
