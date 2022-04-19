61353767c50de8d36891f57cf897ccff505fe46c
brytebee <brytebee@gmail.com>
2022-04-19 16:14:38 +0100

    :white_check_mark: Add GenreList in list_genre.rb
    Added GenreList in list_genre.rb

---
+++ modules/list_genre.rb
@@ -0,0 +1,13 @@
+module GenreList
+  def list_all_genres
+    if @genres.length.zero?
+      puts 'Database is empty'
+    else
+      puts 'List of Genre: '
+      @genres.each_with_index do |genre, index|
+        puts "#{index}: Label: Name: #{genre.name}"
+      end
+
+    end
+  end
+end
