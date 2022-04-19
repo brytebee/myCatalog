9f19a4afd63da651c9c5a104a72352912b380147
brytebee <brytebee@gmail.com>
2022-04-19 11:18:20 +0100

    :test_tube: Complete genere tests
    Completed genere tests

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -8,9 +8,8 @@ describe Genre do
       @gen = Genre.new('Fiction')
       today = Date.today
       twelve_years = Date.new(today.year - 12, today.month, today.day)
-      item = Item.new(twelve_years)
-      item2 = Item.new(today)
-      p @gen
+      @item = Item.new(twelve_years)
+      @item2 = Item.new(today)
     end
 
     it 'should return an instance of Genre class' do
@@ -19,6 +18,35 @@ describe Genre do
 
     it 'return the length of items' do
       expect(@gen.items.length).to be(0)
+    end
+
+    it 'return the length of items' do
+      @gen.add_item(@item)
+      expect(@gen.items.length).to eq(1)
+    end
+
+    it 'object is not an instance of class Item' do
+      expect(@gen).not_to be_kind_of Item
+    end
+
+    it 'instance of name' do
+      instance_property = @gen.name
+      expect(instance_property).to eq('Fiction')
+    end
+    
+    it 'instance of name' do
+      instance_property = @gen.id
+      expect(instance_property).not_to eq(-100)
+    end
+
+    it 'Reject duplicate and still return length == 1' do
+      f = @gen.add_item(@item)
+      expect(@gen.items.length).to eq(1)
+    end
+    
+    it 'Accept another item' do
+      f = @gen.add_item(@item2)
+      expect(@gen.items.length).to eq(2)
     end
   end
 end
