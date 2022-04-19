28c0051042d2b6e45a05dab24054f6b2e05f732a
brytebee <brytebee@gmail.com>
2022-04-19 22:09:27 +0100

    :white_check_mark: Add save genre
    Added save genre

--- modules/save.rb
+++ modules/save.rb
@@ -7,4 +7,13 @@ module PreserveMusicAlbumGenre
 
     File.write('./store/music_album.json', JSON.generate(data))
   end
+
+  def save_genre
+    data = []
+    @genres.each do |genre|
+      data << { name: genre.name }
+    end
+
+    File.write('./store/genre.json', JSON.generate(data))
+  end
 end
