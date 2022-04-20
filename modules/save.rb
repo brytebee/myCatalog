629ab53b2f04d622cf8d4036bb2adc1fc1b5360f
brytebee <brytebee@gmail.com>
2022-04-20 11:53:31 +0100

    :recycle: Reviewer suggested changes
    Changed pulish_date to publisih_date

--- modules/save.rb
+++ modules/save.rb
@@ -2,7 +2,7 @@ module PreserveMusicAlbumGenre
   def save_music_album
     data = []
     @music_albums.each do |music_album|
-      data << { pulish_date: music_album.publish_date, on_spotify: music_album.on_spotify }
+      data << { publish_date: music_album.publish_date, on_spotify: music_album.on_spotify }
     end
 
     File.write('./store/music_album.json', JSON.generate(data))
