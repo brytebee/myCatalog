13160695da582a638f02b9296d59c6443b729d85
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-20 18:00:49 +0200

    dd to_json module to Author class

--- classes/author.rb
+++ classes/author.rb
@@ -13,4 +13,12 @@ class Author
     @items << item
     item.author = self
   end
+
+  def to_json(*args)
+    {
+      'author' => self.class.name,
+      'first_name' => @first_name,
+      'last_name' => @last_name
+    }.to_json(*args)
+  end
 end
