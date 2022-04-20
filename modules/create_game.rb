106d9452fe950dee2ff270e5d2957facecf379bd
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-20 13:11:19 +0200

    Allow recovery of app if user enters wrong date format

--- modules/create_game.rb
+++ modules/create_game.rb
@@ -4,10 +4,16 @@ module GameCreator
     print 'Is this game multiplayer(Y/N): '
     multiplayer = gets.chomp
     multiplayer = %w[y Y].include?(multiplayer)
-    print 'Enter last date the game was played [Enter date in format (yyyy-mm-dd)]: '
-    last_played_at = gets.chomp
-    print 'Enter the date of publish [Enter date in format (yyyy-mm-dd)]: '
-    publish_date = gets.chomp
+    loop do
+      print 'Enter last date the game was played [Enter date in format (yyyy-mm-dd)]: '
+      last_played_at = gets.chomp
+      Date.parse(last_played_at)
+      print 'Enter the date of publish [Enter date in format (yyyy-mm-dd)]: '
+      publish_date = gets.chomp
+      Date.parse(publish_date)
+    rescue StandardError
+      print "Invalid date entered. Please insert in the format specified \n"
+    end
     @games << Game.new(multiplayer, last_played_at, publish_date)
     puts 'Game created successfully.'
   end
