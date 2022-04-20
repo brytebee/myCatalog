6f36fefc51a4e91ac5084a231cd820b4a417e8f6
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-20 13:11:52 +0200

    Change app class to include add_author method

--- classes/app.rb
+++ classes/app.rb
@@ -5,8 +5,10 @@ require_relative './label'
 require_relative './music_album'
 require_relative './genre'
 require_relative './game'
+require_relative './author'
 require_relative '../modules/create_book'
 require_relative '../modules/create_game'
+require_relative '../modules/create_author'
 require_relative '../modules/list_books'
 require_relative '../modules/list_authors'
 require_relative '../modules/list_labels'
@@ -17,6 +19,7 @@ require_relative '../json/label_json'
 class App
   include CreateBook
   include GameCreator
+  include AuthorCreator
   include LabelList
   include GameList
   include AuthorList
@@ -36,7 +39,7 @@ class App
     puts 'Welcome to my Catalog of Things App!'
     until list_of_options
       input = gets.chomp
-      if input == '10'
+      if input == '11'
         puts 'Good Bye!'
         break
       end
@@ -66,8 +69,10 @@ class App
       add_music_album
     when '9'
       add_game
+    when '10'
+      add_author
     else
-      puts 'Please enter a number between 1 and 10.'
+      puts 'Please enter a number between 1 and 11s.'
       puts
     end
     # rubocop:enable Metrics
--- modules/list_authors.rb
+++ modules/list_authors.rb
@@ -5,7 +5,7 @@ module AuthorList
     else
       puts 'List of all Authors: '
       @authors.each_with_index do |author, index|
-        puts "#{index}) [Author] First Name: #{author.publish_date}, Last Name: #{author.last_played_at} "
+        puts "#{index}) [Author] First Name: #{author.first_name}, Last Name: #{author.last_name} "
       end
     end
   end
