858520343647649b53973515464121e5652c18b6
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-21 15:37:13 +0200

    Add game relationship visually in the console for the user

--- modules/list_authors.rb
+++ modules/list_authors.rb
@@ -6,6 +6,8 @@ module AuthorList
       puts 'List of all Authors: '
       @authors.each_with_index do |author, index|
         puts "#{index}) [Author] First Name: #{author.first_name}, Last Name: #{author.last_name} "
+        puts "This author has the following Games in this collection:
+        #{author.items.map(&:id)}"
       end
     end
   end
