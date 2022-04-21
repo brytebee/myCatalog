6635f8d1c978a88332dc970cab7c616285d26914
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-21 15:35:44 +0200

    Two changes below
    - Allow author ID to have a default value or be given in initialisation
    - Modify to_json method on Author class to save game_Id's of Game objects the author has relationship with

--- classes/author.rb
+++ classes/author.rb
@@ -2,8 +2,8 @@ class Author
   attr_accessor :first_name, :last_name
   attr_reader :id, :items
 
-  def initialize(first_name, last_name)
-    @id = rand(1..1000)
+  def initialize(first_name, last_name, id = rand(1..1000))
+    @id = id
     @first_name = first_name
     @last_name = last_name
     @items = []
@@ -14,11 +14,17 @@ class Author
     item.author = self
   end
 
+  def item_ids
+    @items.map(&:id)
+  end
+
   def to_json(*args)
     {
       'author' => self.class.name,
       'first_name' => @first_name,
-      'last_name' => @last_name
+      'last_name' => @last_name,
+      'id' => @id,
+      'game_ids' => item_ids
     }.to_json(*args)
   end
 end
